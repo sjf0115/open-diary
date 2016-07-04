b9f075274e9d19743f70ed07db43517cb4ecb18f
jifeng.si <jifeng.si@qunar.com>
2016-07-04 14:51:24 +0800

    elasticsearch----stats,sum

--- ElasticSearchDemo/src/main/java/com/sjf/open/api/Aggregations.java
+++ ElasticSearchDemo/src/main/java/com/sjf/open/api/Aggregations.java
@@ -11,7 +11,11 @@ import org.elasticsearch.search.aggregations.bucket.terms.LongTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.metrics.MetricsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.avg.Avg;
+import org.elasticsearch.search.aggregations.metrics.max.Max;
 import org.elasticsearch.search.aggregations.metrics.min.Min;
+import org.elasticsearch.search.aggregations.metrics.stats.Stats;
+import org.elasticsearch.search.aggregations.metrics.sum.Sum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -75,7 +79,7 @@ public class Aggregations {
 
     /**
      * https://www.elastic.co/guide/en/elasticsearch/client/java-api/current/_metrics_aggregations.html 最小值
-     *  最小值
+     *  聚合之min
      * @param client
      * @param index
      * @param type
@@ -101,8 +105,124 @@ public class Aggregations {
         logger.info("员工中最小年龄 {} 聚合名称 {}",value,agg.getName());
     }
 
+    /**
+     *  聚合之max
+     * @param client
+     * @param index
+     * @param type
+     */
+    public static void maxAggregation(Client client, String index, String type) {
+
+        // 聚合条件
+        MetricsAggregationBuilder maxAggregation = AggregationBuilders.max("max_age").field("age");
+
+        // 聚合
+        SearchRequestBuilder requestBuilder = client.prepareSearch();
+        requestBuilder.setIndices(index);
+        requestBuilder.setTypes(type);
+        requestBuilder.addAggregation(maxAggregation);
+
+        // 执行
+        SearchResponse searchResponse = requestBuilder.execute().actionGet();
+
+        // 结果
+        Max agg = searchResponse.getAggregations().get("max_age");
+        double value = agg.getValue();
+
+        logger.info("员工中最大年龄 {} 聚合名称 {}",value,agg.getName());
+    }
+
+    /**
+     *  聚合之sum
+     * @param client
+     * @param index
+     * @param type
+     */
+    public static void sumAggregation(Client client, String index, String type) {
+
+        // 聚合条件
+        MetricsAggregationBuilder sumAggregation = AggregationBuilders.sum("sum_age").field("age");
+
+        // 聚合
+        SearchRequestBuilder requestBuilder = client.prepareSearch();
+        requestBuilder.setIndices(index);
+        requestBuilder.setTypes(type);
+        requestBuilder.addAggregation(sumAggregation);
+
+        // 执行
+        SearchResponse searchResponse = requestBuilder.execute().actionGet();
+
+        // 结果
+        Sum agg = searchResponse.getAggregations().get("sum_age");
+        double value = agg.getValue();
+
+        logger.info("员工总年龄 {} 聚合名称 {}",value,agg.getName());
+    }
+
+    /**
+     *  聚合之avg
+     * @param client
+     * @param index
+     * @param type
+     */
+    public static void avgAggregation(Client client, String index, String type) {
+
+        // 聚合条件
+        MetricsAggregationBuilder avgAggregation = AggregationBuilders.avg("avg_age").field("age");
+
+        // 聚合
+        SearchRequestBuilder requestBuilder = client.prepareSearch();
+        requestBuilder.setIndices(index);
+        requestBuilder.setTypes(type);
+        requestBuilder.addAggregation(avgAggregation);
+
+        // 执行
+        SearchResponse searchResponse = requestBuilder.execute().actionGet();
+
+        // 结果
+        Avg agg = searchResponse.getAggregations().get("avg_age");
+        double value = agg.getValue();
+
+        logger.info("员工平均年龄 {} 聚合名称 {}",value,agg.getName());
+    }
+
+    /**
+     *  聚合之stats
+     * @param client
+     * @param index
+     * @param type
+     */
+    public static void statsAggregation(Client client, String index, String type) {
+
+        // 聚合条件
+        MetricsAggregationBuilder statsAggregation = AggregationBuilders.stats("stats_age").field("age");
+
+        // 聚合
+        SearchRequestBuilder requestBuilder = client.prepareSearch();
+        requestBuilder.setIndices(index);
+        requestBuilder.setTypes(type);
+        requestBuilder.addAggregation(statsAggregation);
+
+        // 执行
+        SearchResponse searchResponse = requestBuilder.execute().actionGet();
+
+        // 结果
+        Stats agg = searchResponse.getAggregations().get("stats_age");
+        double min = agg.getMin();
+        double max = agg.getMax();
+        double avg = agg.getAvg();
+        double sum = agg.getSum();
+        long count = agg.getCount();
+
+        logger.info("员工最小年龄 {} 最大年龄 {} 平均年龄 {} 总年龄 {} 人数 {} 聚合名称 {}",min, max, avg, sum, count, agg.getName());
+    }
+
     public static void main(String[] args) {
         Client client = Common.createClient();
-        minAggregation(client,INDEX,TYPE);
+        //minAggregation(client,INDEX,TYPE);
+        //maxAggregation(client,INDEX,TYPE);
+        //sumAggregation(client,INDEX,TYPE);
+        //avgAggregation(client,INDEX,TYPE);
+        statsAggregation(client,INDEX,TYPE);
     }
 }
